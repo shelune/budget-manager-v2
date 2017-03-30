71800a5a383a8d5c50de6fd88aee93a34ceabda5
shelune <hieu@mios.fi>
2017-03-30 19:46:57 +0300

    heroku af

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
-const databaseName = 'budgets';
+const databaseName = 'heroku_cmd9919z';
 
 module.exports = {
     connectionUrl: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${databaseName}`
