76a90a670f726538e3524d8038c7075f597db9ef
shelune <hieu@mios.fi>
2017-03-30 19:16:54 +0300

    fucking init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules": {
+        "require-jsdoc": "warn",
+        "comma-dangle": "off",
+        "max-len": "warn",
+        "new-cap": "off"
+    }
+};
---
+++ api/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
